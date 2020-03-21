ee3d6cd87371dc9d0f9fd88525e65889a2783ad1
mabdo913 <magarsa.abdo@gmail.com>
2020-03-21 12:47:17 -0600

    DOM exercise (incomplete)

---
+++ 02-dom/domfunc.js
@@ -0,0 +1,45 @@
+
+
+const DOMFunctions = {
+
+    createListItem: (itemNum) => {     
+
+      const listItem = document.createElement("LI");       
+      const textnode = document.createTextNode("Item " + itemNum); 
+      listItem.appendChild(textnode);
+
+      return listItem;
+    },
+
+    createCard: () => {
+        //create elements we need 
+        const div = document.createElement("div");
+        const h4 = document.createElement("h4");
+        const beforeButton = document.createElement("button");
+        const afterButton = document.createElement("button");
+        const deleteButton = document.createElement("button");
+        const beforeText = document.createTextNode("Add Before");
+        const afterText = document.createTextNode("Add After");  
+        const delText = document.createTextNode("Delete"); 
+        const cardText = document.createTextNode("Card "); 
+
+        //attach buttons and h4 to div
+        div.appendChild(h4); div.appendChild(beforeButton); div.appendChild(afterButton); div.appendChild(deleteButton);
+
+        //attach text to elements
+        div.append(cardText); h4.append(cardText); beforeButton.append(beforeText); afterButton.append(afterText); deleteButton.append(delText); 
+
+        //add style to card
+        div.className = 'card'; div.style.border = "2px solid black";
+
+        return div;
+    },
+
+    updateCards: (nodeArray) => {
+
+    }
+
+    
+}
+
+export default DOMFunctions;
--- 02-dom/main.js
+++ 02-dom/main.js
@@ -1,3 +1,28 @@
-container.addEventListener('click', e (() => {
-   console.log(e); 
-}));
+import DOMFunctions from './domfunc.js';
+
+addButton.addEventListener("click", () => {
+   const ol = document.getElementById("itemList");
+   const li = DOMFunctions.createListItem(ol.childElementCount+1);
+   document.getElementById("itemList").appendChild(li);   
+});
+
+document.body.addEventListener("click", e => {
+   const parent = e.target.parentNode, buttonText = e.target.textContent, cardGroup = e.target.parentNode.parentNode.parentNode; 
+   //const cardArray = Array.from(cardGroup.children); 
+   //console.log(cardArray);
+
+   if (e.target.nodeName == 'BUTTON') {
+         if (buttonText == 'Delete') {
+           parent.remove();                  
+         }
+         if (buttonText == 'Add Before') {
+            const newCard = DOMFunctions.createCard();            
+            parent.insertAdjacentElement('beforebegin',newCard); 
+            //domfunc.updateCards(parent);           
+         }
+         if (buttonText == 'Add After') {
+            const newCard = DOMFunctions.createCard();            
+            parent.insertAdjacentElement('afterend',newCard); 
+         }      
+   } 
+});
