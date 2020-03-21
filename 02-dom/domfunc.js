bc5ce6500ccc934f3d3c98b242ca583c9ad73336
mabdo913 <magarsa.abdo@gmail.com>
2020-03-21 14:28:21 -0600

    completed DOM exercise

--- 02-dom/domfunc.js
+++ 02-dom/domfunc.js
@@ -11,7 +11,7 @@ const DOMFunctions = {
       return listItem;
     },
 
-    createCard: () => {
+    createCard: cardNum => {
         //create elements we need 
         const div = document.createElement("div");
         const h4 = document.createElement("h4");
@@ -21,7 +21,7 @@ const DOMFunctions = {
         const beforeText = document.createTextNode("Add Before");
         const afterText = document.createTextNode("Add After");  
         const delText = document.createTextNode("Delete"); 
-        const cardText = document.createTextNode("Card "); 
+        const cardText = document.createTextNode("Card " + cardNum); 
 
         //attach buttons and h4 to div
         div.appendChild(h4); div.appendChild(beforeButton); div.appendChild(afterButton); div.appendChild(deleteButton);
@@ -33,11 +33,7 @@ const DOMFunctions = {
         div.className = 'card'; div.style.border = "2px solid black";
 
         return div;
-    },
-
-    updateCards: (nodeArray) => {
-
-    }
+    }   
 
     
 }
--- 02-dom/main.js
+++ 02-dom/main.js
@@ -7,22 +7,20 @@ addButton.addEventListener("click", () => {
 });
 
 document.body.addEventListener("click", e => {
-   const parent = e.target.parentNode, buttonText = e.target.textContent, cardGroup = e.target.parentNode.parentNode.parentNode; 
-   //const cardArray = Array.from(cardGroup.children); 
-   //console.log(cardArray);
+   const parent = e.target.parentNode, buttonText = e.target.textContent, cardNum = parent.parentNode.childElementCount+1;   
 
    if (e.target.nodeName == 'BUTTON') {
          if (buttonText == 'Delete') {
            parent.remove();                  
          }
          if (buttonText == 'Add Before') {
-            const newCard = DOMFunctions.createCard();            
-            parent.insertAdjacentElement('beforebegin',newCard); 
-            //domfunc.updateCards(parent);           
+            const newCard = DOMFunctions.createCard(cardNum);            
+            parent.insertAdjacentElement('beforebegin',newCard);                       
          }
          if (buttonText == 'Add After') {
-            const newCard = DOMFunctions.createCard();            
-            parent.insertAdjacentElement('afterend',newCard); 
+            const newCard = DOMFunctions.createCard(cardNum);            
+            parent.insertAdjacentElement('afterend',newCard);
+            
          }      
    } 
 });
