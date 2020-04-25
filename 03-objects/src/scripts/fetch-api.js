8ed24b4d65e24ec01b9ebda73b23f5b796ce87ca
mabdo <magarsa.abdo@icloud.com>
2020-04-25 17:04:24 -0600

    completed  fetch exercises (comp 920)

--- 03-objects/src/scripts/fetch-api.js
+++ 03-objects/src/scripts/fetch-api.js
@@ -2,7 +2,14 @@ const fetch = require("node-fetch");
 
 const fetchFunctions = {
 
-    url: 'https://jsonplaceholder.typicode.com/users',
+    me: {
+        "id": 1,
+        "name": "Mags Abdo",
+        "username": "mabdo913",
+        "email": "mabdo913@420.biz"        
+        },    
+
+    url: 'https://jsonplaceholder.typicode.com/users',    
 
     getFirstName(data) {        
         return data[0].name;
@@ -27,7 +34,29 @@ const fetchFunctions = {
         const data = await fetchFunctions.getUsers();
         console.log(fetchFunctions.getFirstName(data));
         console.log(fetchFunctions.getAllFirstNames(data));
-    }
+    },
+
+    async postData(url = '', data = {}) {
+        // Default options are marked with *
+        const response = await fetch(url, {
+            method: 'POST',     // *GET, POST, PUT, DELETE, etc.
+            mode: 'cors',       // no-cors, *cors, same-origin
+            cache: 'no-cache',  // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: 'same-origin', // include, *same-origin, omit
+            headers: {
+                'Content-Type': 'application/json'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            redirect: 'follow',         // manual, *follow, error
+            referrer: 'no-referrer',    // no-referrer, *client
+            body: JSON.stringify(data)  // body data type must match "Content-Type" header
+        });
+
+        const json = await response.json();   // parses JSON response into native JavaScript objects
+        json.status = response.status;
+        json.statusText = response.statusText;
+        return json;
+}
 
 
 } 
--- 03-objects/src/scripts/fetch-api.test.js
+++ 03-objects/src/scripts/fetch-api.test.js
@@ -1,6 +1,12 @@
 import fetchFunctions from './fetch-api.js';
 
-test('Check to see if async function works...', async (done)=> {
-    await fetchFunctions.workWithData(); 
+test('Check to see if async function "workingWithData()" works...', async (done)=> {
+    await fetchFunctions.workWithData(); // logs first name and all 10 names to the console
     done();   
+});
+
+test('Check values of variables', async (done) => {    
+    const json = await fetchFunctions.postData(fetchFunctions.url, fetchFunctions.me);
+    console.log('Status code:', json.status, 'Status text:', json.statusText); // logs 'Status code: 201 Status text: Created' to the console   
+    done(); 
 });
