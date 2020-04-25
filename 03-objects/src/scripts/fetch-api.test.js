9fdbfe2eebd36cd7420ac4c0c8d4292cd04e8f7c
mabdo <magarsa.abdo@icloud.com>
2020-04-25 17:13:31 -0600

    refactored fetch-api test

--- 03-objects/src/scripts/fetch-api.test.js
+++ 03-objects/src/scripts/fetch-api.test.js
@@ -5,7 +5,7 @@ test('Check to see if async function "workingWithData()" works...', async (done)
     done();   
 });
 
-test('Check values of variables', async (done) => {    
+test('Check to see if function "postData()" function works...', async (done) => {    
     const json = await fetchFunctions.postData(fetchFunctions.url, fetchFunctions.me);
     console.log('Status code:', json.status, 'Status text:', json.statusText); // logs 'Status code: 201 Status text: Created' to the console   
     done(); 
