0a106efb4f4c25130a3183eccc3ef7e7e36015c5
mabdo913 <magarsa.abdo@gmail.com>
2020-05-07 21:09:04 -0600

    write more tests forCity class

--- 03-objects/src/scripts/city.js
+++ 03-objects/src/scripts/city.js
@@ -3,15 +3,49 @@ const oopCity = {};
 class City {
     constructor(name, lat, lon, pop) {
         this.name = name,
-        this.lat = parseFloat(lat.toFixed(2)),
-        this.lon = parseFloat(lon.toFixed(2)),
+        this.lat = parseFloat(parseFloat(lat).toFixed(2)),
+        this.lon = parseFloat(parseFloat(lon).toFixed(2)),
         this.pop = pop;
     }
 
     show() {
-        return `Name: ${this.name}, Latitude: ${this.lat}, Longitude: ${this.lon}, Population: ${this.pop} million`;
+        return `Name: ${this.name}, Latitude: ${this.lat}, Longitude: ${this.lon}, Population: ${this.pop}`;
     }
+
+    movedIn(num) {
+        this.pop = this.pop + num;
+    }
+
+    movedOut(num) {
+        this.pop = this.pop - num;
+    }
+
+    howBig() {   
+        const population = this.pop;
+        let str = '';
+
+        if (population > 100000) {
+            str = 'City';
+        } else if (population > 20000 && population < 100000) {
+            str = 'Large Town';
+        } else if (population > 1000 && population < 20000) {
+            str = 'Town';
+        } else if (population > 100 && population < 1000) {
+            str = 'Village';
+        } else if (population < 100) {
+            str = 'Hamlet';
+        }
+        return str;       
+    }
+
+    whichSphere() {
+        
+    }
+
+
 }
+
+    
 
 class Community {
     
--- 03-objects/src/scripts/city.test.js
+++ 03-objects/src/scripts/city.test.js
@@ -1,24 +1,80 @@
 import cityStuff from './city.js';
 
 test('Test OOP for City class', ()=>{
-    const london = new cityStuff.City('London', 51.51, -0.12, 8.98);
+    const london = new cityStuff.City('London', 51.51, -0.12, 8987400);
 
     expect(london.name).toBe('London');
     expect(london.lat).toBe(51.51);
     expect(london.lon).toBe(-0.12);
-    expect(london.pop).toBe(8.98);
+    expect(london.pop).toBe(8987400);
 });
 
-test('Test OOP show method City class', () => {
+test('Test OOP show method in City class', () => {    
     const cities = [];
-    cities.push(new cityStuff.City('New York', 40.71, -74.01, 8.41));
-    cities.push(new cityStuff.City('Los Angeles', 34.05, -118.24, 3.88));
+    cities.push(new cityStuff.City('New York', 40.71, -74.01, 8419600));
+    cities.push(new cityStuff.City('Los Angeles', 34.05, -118.24, 3881450));
 
     expect(cities[0].show()).toBe(
-            "Name: New York, Latitude: 40.71, Longitude: -74.01, Population: 8.41 million"
+            "Name: New York, Latitude: 40.71, Longitude: -74.01, Population: 8419600"
         );
 
     expect(cities[1].show()).toBe(
-            "Name: Los Angeles, Latitude: 34.05, Longitude: -118.24, Population: 3.88 million"
+            "Name: Los Angeles, Latitude: 34.05, Longitude: -118.24, Population: 3881450"
         );
+});
+
+test('Test OOP movedIn method in City class', () => {    
+    const cities = [];
+    cities.push(new cityStuff.City('New York', 40.71, -74.00, 8419600));
+    cities.push(new cityStuff.City('Los Angeles', 34.05, -118.24, 3881450));
+
+    cities[0].movedIn(65000);
+    expect(cities[0].pop).toBe(8484600);
+
+    cities[1].movedIn(120000);
+    expect(cities[1].pop).toBe(4001450);
+});
+
+test('Test OOP movedOut method in City class', () => {    
+    const cities = [];
+    cities.push(new cityStuff.City('New York', 40.71, -74.01, 8419600));
+    cities.push(new cityStuff.City('Los Angeles', 34.05, -118.24, 3881450));
+
+    cities[0].movedOut(65000);
+    expect(cities[0].pop).toBe(8354600);
+
+    cities[1].movedOut(120000);
+    expect(cities[1].pop).toBe(3761450);
+});
+
+test('Test OOP howBig method in City class', () => {    
+    const cities = [];
+    cities.push(new cityStuff.City('Sylvan Lake', 52.31, -114.10, 14816));
+    cities.push(new cityStuff.City('Cochrane', 51.19, -114.47, 25853));
+    cities.push(new cityStuff.City('London', 51.51, -0.12, 8987400));
+    cities.push(new cityStuff.City('Churchill', 58.77, -94.16, 899));
+    cities.push(new cityStuff.City('Tilt Cove', 49.88, -55.62, 5));
+
+    expect(cities[0].howBig()).toBe("Town");
+    expect(cities[1].howBig()).toBe("Large Town");
+    expect(cities[2].howBig()).toBe("City");
+    expect(cities[3].howBig()).toBe("Village");
+    expect(cities[4].howBig()).toBe("Hamlet");
+});
+
+test('Test OOP whichSphere method in City class', () => {    
+    const cities = [];
+    cities.push(new cityStuff.City('Sao Paulo', -23.53, -46.62, 12176866));
+    cities.push(new cityStuff.City('New York', 40.71, -74.01, 8419600));    
+    cities.push(new cityStuff.City('Jakarta', -6.20, 106.02, 10770487));
+    cities.push(new cityStuff.City('Los Angeles', 34.05, -118.24, 3881450));
+    cities.push(new cityStuff.City('Johannesburg', -26.20, 28.03, 4434800));
+    cities.push(new cityStuff.City('London', 51.51, -0.12, 8987400));
+
+    expect(cities[0].whichSphere()).toBe("Southern Hemisphere");
+    expect(cities[1].whichSphere()).toBe("Northern Hemisphere");
+    expect(cities[2].whichSphere()).toBe("Southern Hemisphere");
+    expect(cities[3].whichSphere()).toBe("Northern Hemisphere");
+    expect(cities[4].whichSphere()).toBe("Southern Hemisphere");
+    expect(cities[5].whichSphere()).toBe("Northern Hemisphere");    
 });
